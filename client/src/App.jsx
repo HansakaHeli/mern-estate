a23103bbedf6a1501787f7355e3c2c3d008ba0f4
HansakaHeli <programminghhr66@gmail.com>
2024-07-28 22:37:18 +0530

    Delete nodemodules

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,23 @@
 import React from 'react'
 import {BrowserRouter, Routes, Routs} from 'react-router-dom'
 import Home from './pages/Home'
-import Signin from './pages/Signin'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
-import About from './pages/about'
+import Header from './components/Header'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
 
 export default function App() {
   return (
     <BrowserRouter>
-
+      <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/sign-in" element={<Signin/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
         <Route path="/sign-up" element={<SignUp/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/profile" element={<Profile/>}/>
-
       </Routes>
-
     </BrowserRouter>
   )
 }
