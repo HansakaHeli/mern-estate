74df649984aecc3ed24666176a779f690a785707
HansakaHeli <programminghhr66@gmail.com>
2024-09-01 20:47:11 +0530

    Complete sign up page funtionalities

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function SignUp() {
 
   const [formData, setFormData] = useState({})
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleChange = (e) =>{
     setFormData(
@@ -18,28 +19,38 @@ export default function SignUp() {
 
   const handleSubmit = async (e) =>{
     e.preventDefault(); // Prevent the refresh of the page
-    setLoading(true);
-    const res = await fetch('/api/auth/signup',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
 
-        },
-        body: JSON.stringify(formData), // Convert the formData object to a JSON string and include it in the request body.
+    try {
+
+      setLoading(true);
+      const res = await fetch('/api/auth/signup',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+
+          },
+          body: JSON.stringify(formData), // Convert the formData object to a JSON string and include it in the request body.
+        }
+      );
+      const data = await res.json(); // Parse the response from the server as JSON.
+      console.log(data);
+      if(data.success === false){
+        setLoading(false);
+        setError(data.message);
+        return;
       }
-    );
-    const data = await res.json(); // Parse the response from the server as JSON.
-    if(data.success === false){
-      setError(data.message);
       setLoading(false);
-      return;
+      setError(null);
+      navigate("/sign-in");
+      
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
     }
-    setLoading(false);
-    console.log(data);
-  }
 
-  console.log(formData);
+    
+  }
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -48,7 +59,9 @@ export default function SignUp() {
         <input type='text' placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
         <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email'onChange={handleChange}/>
         <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password'onChange={handleChange}/>
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign up</button>
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+          {loading ? 'Loading...' : 'Sign Up'}
+        </button>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an acccount?</p>
@@ -56,6 +69,7 @@ export default function SignUp() {
           <span className='text-blue-700'>Sign in</span>
         </Link>
       </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   )
 }
